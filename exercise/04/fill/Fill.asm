20c6aa108251b4e98713bc279bb2ff70fd35d7d0
TKTim <36975354+TKTim@users.noreply.github.com>
2018-11-29 12:53:46 +0800

    Update Fill.asm

--- exercise/04/fill/Fill.asm
+++ exercise/04/fill/Fill.asm
@@ -13,50 +13,64 @@
 
 // Put your code here.
 
- @status
-    M=-1        // status=0xFFFF
-    D=0         // Argument - what to set screen bits to
-    @SETSCREEN
-    0;JMP
+ (FOREVER)
+// arr = SCREEN
+	@SCREEN
+	D=A
+	@arr
+	M=D
 
+// n=8192
+	@8192
+	D=A
+	@n
+	M=D
+
+	@i
+	M=0
 (LOOP)
-    @KBD
-    D=M         // D = current keyboard character
-    @SETSCREEN
-    D;JEQ       // If no key, set screen to zeroes (white)
-    D=-1        // If key pressed, set scr   een to all 1 bits (black)
-    
-(SETSCREEN)     // Set D=new status before jumping here
-    @ARG
-    M=D         // Save new status arg
-    @status     // FFFF=black, 0=white - status of entire screen
-    D=D-M       // D=newstatus-status
-    @LOOP
-    D;JEQ       // Do nothing if new status == old status
-    
-    @ARG        //return value 
-    D=M
-    @status
-    M=D         // status = ARG
-    
-    @SCREEN
-    D=A         // D=Screen address
-    @8192
-    D=D+A       // D=Byte just past last screen address
-    @i
-    M=D         // i=SCREEN address
-    
-(SETLOOP)    
-    @i
-    D=M-1
-    M=D         // i=i-1
-    @LOOP
-    D;JLT       // if i<0 goto LOOP
-    
-    @status
-    D=M         // D=status
-    @i
-    A=M         // Indirect
-    M=D         // M[current screen address]=status
-    @SETLOOP
-    0;JMP
+  // if (i==n) goto ENDLOOP
+	@i
+	D=M
+	@n
+	D=D-M
+	@ENDLOOP
+	D; JEQ
+	
+  // if (*KBD != 0)
+	@KBD
+	D=M     // D = *KBD
+	@ELSE
+	D; JEQ  // if (*KDB==0) goto ELSE
+	
+	//   RAM[arr+i] = -1
+	@arr
+	D=M
+	@i
+	A=D+M
+	M=-1
+	
+	@ENDIF
+	0; JMP
+(ELSE)	
+  // else 
+  //   RAM[arr+i] = 0
+	@arr
+	D=M
+	@i
+	A=D+M
+	M=0
+	
+(ENDIF)
+	
+	// i++
+	@i
+	M=M+1
+	
+	@LOOP
+	0; JMP
+
+(ENDLOOP)
+	@FOREVER
+	0; JMP
+	
